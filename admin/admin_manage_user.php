6d4c788394885dc6d908e08ea52e651cfba5e42b
limjiade1129 <limjiadedede@gmail.com>
2024-11-27 23:50:09 +0800

    .

--- admin/admin_manage_user.php
+++ admin/admin_manage_user.php
@@ -24,6 +24,7 @@ $has_events = mysqli_num_rows($user_result) > 0;
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script> <!-- For sweet alert modal -->
+    
     <style>
         body {
             font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
